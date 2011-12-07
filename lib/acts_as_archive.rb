9ae701247614da3d615d8afd048feef4c3b34e5d
John Weathers <johnweathers@gmail.com>
2011-12-07 18:26:50 -0500

    Fix deprecation warning in Rails 3.1

--- lib/acts_as_archive.rb
+++ lib/acts_as_archive.rb
@@ -139,7 +139,7 @@ class ActsAsArchive
             if association.options[:dependent] && ActsAsArchive.find(association.klass)
               opts = association.options.dup
               opts[:class_name] = "::#{association.class_name}::Archive"
-              opts[:foreign_key] = association.primary_key_name
+              opts[:foreign_key] = association.foreign_key
               klass.send association.macro, association.name, opts
             end
           end
